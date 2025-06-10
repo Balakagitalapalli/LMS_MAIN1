b3992bd2ee1a8d4333141a7359e2b156429aa807
Bala Kagitalapalli <147422387+Balakagitalapalli@users.noreply.github.com>
2025-06-10 13:49:23 +0530

    Update AddQuestions.jsx

--- frontend/src/Components/AddQuestions.jsx
+++ frontend/src/Components/AddQuestions.jsx
@@ -71,7 +71,7 @@ function AddQuestion() {
     
 
     try {
-      const response = await fetch('http://localhost:8080/api/questions', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
