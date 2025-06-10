621904266223c70ab04111e27bc04942678b23ce
Bala Kagitalapalli <147422387+Balakagitalapalli@users.noreply.github.com>
2025-06-10 13:48:38 +0530

    Update AddCourse.jsx

--- frontend/src/Components/AddCourse.jsx
+++ frontend/src/Components/AddCourse.jsx
@@ -22,7 +22,7 @@ function AddCourse() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8080/api/courses', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
