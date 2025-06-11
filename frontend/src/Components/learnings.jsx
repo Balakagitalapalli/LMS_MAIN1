a851d03416690df2ee5888e95ce22ef81e599e9e
Bala Kagitalapalli <147422387+Balakagitalapalli@users.noreply.github.com>
2025-06-11 18:57:57 +0530

    Update learnings.jsx

--- frontend/src/Components/learnings.jsx
+++ frontend/src/Components/learnings.jsx
@@ -11,7 +11,7 @@ function Learnings(){
     useEffect(() => {
         async function fetchCourse() {
           try {
-            const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/learning/${userId}`);
+            const response = await  axios.post(`https://inspiring-balance-production.up.railway.app/api/learning/${userId}`);
             const fetchedCourse = response.data;
             setCourse(fetchedCourse);
           } catch (err) {
