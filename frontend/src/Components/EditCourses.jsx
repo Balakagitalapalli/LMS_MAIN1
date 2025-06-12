7cc329c0a564ad4a2dc621a4793efce44c74fb08
Bala Kagitalapalli <147422387+Balakagitalapalli@users.noreply.github.com>
2025-06-12 10:00:39 +0530

    Update EditCourses.jsx

--- frontend/src/Components/EditCourses.jsx
+++ frontend/src/Components/EditCourses.jsx
@@ -53,7 +53,7 @@ function EditCourse() {
   useEffect(() => {
     async function fetchCourse() {
       try {
-        const response = await  axios.post(`https://inspiring-balance-production.up.railway.app/api/courses/${courseId}`);
+        const response = await  axios.get(`https://inspiring-balance-production.up.railway.app/api/courses/${courseId}`);
         const fetchedCourse = response.data;
         setFormData(fetchedCourse);
       } catch (err) {
@@ -73,7 +73,8 @@ function EditCourse() {
       }
     }
     console.log(formData)
-    const response = await  axios.post(`https://inspiring-balance-production.up.railway.app/api/courses/${courseId}`,
+    const response = await  axios.post(
+      `https://inspiring-balance-production.up.railway.app/api/courses/${courseId}`,
       formData
     );
 
