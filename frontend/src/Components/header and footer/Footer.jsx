7456b599c23699d622fe74609fa848831f999cab
Bala Kagitalapalli <147422387+Balakagitalapalli@users.noreply.github.com>
2025-06-13 22:33:17 +0530

    Update Footer.jsx

--- frontend/src/Components/header and footer/Footer.jsx
+++ frontend/src/Components/header and footer/Footer.jsx
@@ -14,25 +14,31 @@ function Footer(){
           <li><a href="https://api.whatsapp.com/send/?phone=919989776661&text&type=phone_number&app_absent=0">Contact</a></li>
         </div>
         <div className="footer-col">
-          <h3>Intermediate Courses</h3>
-          <li>Web Development</li>
-          <li>Programming</li>
-          <li>Machine Learning</li>
-          <li>Project Fundamentals</li>
+          <h3>🔹Explore Topics</h3>
+          <li><a href="https://lms-main-1.vercel.app/courses">Python</a></li>
+          <li><a href="https://lms-main-1.vercel.app/courses">java</a></li>
+          <li><a href="https://lms-main-1.vercel.app/courses">Control System</a></li>
+          <li><a href="https://lms-main-1.vercel.app/courses">IOT</a></li>
         </div>
         <div className="footer-col">
-          <h3>Beginner Courses</h3>
-          <li>Web Development</li>
-          <li>Programming</li>
-          <li>Machine Learning</li>
-          <li>Project Fundamentals</li>
+          <h3>🔹 Student Resources</h3>
+          <li>Multiple Courses</li>
+          <li>quizzes</li>
+          <li>Progress Dashboard</li>
+          <li>Certifications</li>
         </div>
         <div className="copyright">
           <p>Copyright ©2025 All rights reserved .</p>
           <div className="pro-links">
-            <FontAwesomeIcon icon={faFacebookF} className="i"/>
-            <FontAwesomeIcon icon={faInstagram} className="i"/>
-            <FontAwesomeIcon icon={faLinkedinIn} className="i"/>
+            <a href="https://www.facebook.com/adityauniversity9" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFacebookF} className="i" />
+            </a>
+            <a href="https://www.instagram.com/aditya_university/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} className="i"/>
+            </a>
+            <a href="https://www.linkedin.com/school/adityauniversity/posts/?feedView=all" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedinIn} className="i"/>
+            </a>
           </div>
         </div>
         </footer>
